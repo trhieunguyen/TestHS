911ea3fe668db79b9656442ce25998f9f3bb8d5e
Hieu Nguyen <97285168+trhieunguyen@users.noreply.github.com>
2022-01-10 11:21:10 +0700

    Add files via upload

---
+++ TestHS.sql
@@ -0,0 +1,39 @@
+--Import Excel file to Microsoft SQL Server 2017
+SELECT * FROM Handshake..Log;
+SELECT * FROM Handshake..Users;
+
+--Get date data based on CreatedAt column
+SELECT CreatedAt, CONVERT(DATE, CreatedAt)
+FROM Handshake..Log;
+
+ALTER TABLE Handshake..Log;
+ADD CreatedAtDate DATE;
+
+UPDATE Handshake..Log
+SET CreatedAtDate = CONVERT(DATE, CreatedAt);
+
+--Retrieve users that have logged in for any 3 consecutive days
+--Approach 1
+WITH CTE as (
+SELECT DISTINCT UserID, CreatedAtDate
+FROM Handshake..Log)
+
+SELECT DISTINCT UserID from (
+SELECT *, CASE WHEN (LAG(CreatedAtDate,1) OVER(PARTITION BY UserID order by CreatedAtDate)) = DATEADD(d, -1,CreatedAtDate) 
+		  THEN 1 ELSE 0 END second_day ,
+          CASE WHEN (LAG(CreatedAtDate,2) OVER(PARTITION BY UserID order by CreatedAtDate)) = DATEADD(d, -2,CreatedAtDate) 
+		  THEN 1 ELSE 0 END third_day
+FROM CTE) a 
+WHERE a.second_day =1 and a.third_day =1;
+--Approach 2
+WITH CTE as (
+SELECT UserID, Col1, ROW_NUMBER() OVER(PARTITION BY UserID, Col1 ORDER BY Col1) as PT
+FROM 
+(SELECT *, DATEADD(d, -ROW_NUMBER() OVER(PARTITION BY UserID ORDER BY CreatedAtDate),CreatedAtDate) as Col1
+FROM (
+SELECT DISTINCT UserID, CreatedAtDate
+FROM Handshake..Log) b) c)
+
+SELECT DISTINCT UserID
+FROM CTE
+WHERE PT >2
